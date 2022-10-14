c0ee002a0840dcec62808a1f82dc0e3eaa03f3d4
hiren patel <hirenpatel0526@yahoo.com>
2022-10-14 09:59:26 -0700

    done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT =  3001;
 const app = express();
+const path = require('path');
 // creating port
 // app.use is asking express to make routes for files in public folder 
 app.use(express.static('public'));
@@ -8,7 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 require('./routes/apiRoute')(app);
-require('./routes/htmlRoute')(app);
+// require('./routes/htmlRoute')(app);
 
 app.listen(3001, ()=> {
     console.log(`server available at localhost${PORT}`);
