431e8ee84d4df86e0fbd5d7a9ea67272fa0c6291
hiren patel <hirenpatel0526@yahoo.com>
2022-10-14 12:44:33 -0700

    fixing error

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const PORT =  3001;
 const app = express();
-const path = require('path');
+// const path = require('path');
+require('./routes/apiRoute')(app);
+require('./routes/htmlRoute')(app);
 // creating port
 // app.use is asking express to make routes for files in public folder 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-require('./routes/apiRoute')(app);
-// require('./routes/htmlRoute')(app);
 
 app.listen(3001, ()=> {
-    console.log(`server available at localhost${PORT}`);
+    console.log(`server available at localhost:${PORT}`);
 })
 
 
