30a3166ba65f982ca90b445bcca28877d22440a6
hiren patel <hirenpatel0526@yahoo.com>
2022-10-15 19:48:35 -0700

    idk

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const db = require('../db/db.json');
 
 const fs = require('fs');
 
@@ -6,13 +7,13 @@ const fs = require('fs');
 
 module.exports = (app) => {
     app.get('/api/notes', (req,res)=>{
-        res.sendFile(path.join(__dirname, '../db/db.JSON'));
+        res.sendFile(path.join(__dirname, '../db/db.json'));
     });
 // app.post will send all the information and sending it back.
     app.post('/api/notes', (req,res)=>{
-        const db= fs.readFileSync('db/db.JSON');
+        const db = fs.readFileSync('db/db.JSON');
         db = JSON.parse(db);
-        res.JSON(db);
+        res.json(db);
         // creating body for the title and text
         const inputNote= {
             title: req.body.title,
@@ -26,11 +27,12 @@ module.exports = (app) => {
 
     // this will removenotes if user press delete button
     app.delete('/api/notes/:id', (req,res)=>{
-        const db = JSON.parse(fs.readFileSync('db/db.JSON'))
+        const db = JSON.parse(fs.readFileSync('db/db.json'))
         const removeNotes = db.filter(item => item.id !== req.params.id);
         fs.writeFileSync('db/db.JSON', JSON.stringify(removeNotes));
         res.JSON(removeNotes);
     })
 
 };
+
 
